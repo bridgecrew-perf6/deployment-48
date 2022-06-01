11bdde3adbd07edfd9e22a05c69baf762a904a69
Kara Ballew <karakeatley@gmail.com>
2022-05-31 21:57:36 -0400

    2

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const path = require('path')
 app.use(express.json())
 app.use(cors())
 
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.js'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'))
   })
 app.use('/js', express.static(path.join(__dirname, 'index.js')))
 
