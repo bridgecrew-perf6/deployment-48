36867e5aaae64c4f56986eea5a923629e4989ca3
Kara Ballew <karakeatley@gmail.com>
2022-05-31 21:41:55 -0400

    fingers crossed

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const path = require('path')
 app.use(express.json())
 app.use(cors())
 
@@ -9,3 +10,6 @@ app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, './index.js'))
   })
 app.use('/js', express.static(path.join(__dirname, 'index.js')))
+
+
+app.listen(process.env.SERVER_PORT, () => console.log(`${process.env.SERVER_PORT}`))
